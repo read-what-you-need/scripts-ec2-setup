924d5be8c06e6f64b3b6322bb9b0035a310c6fdc
soulbliss <deepak.garasangi@gmail.com>
2021-04-10 16:30:27 +0530

    first commit

---
+++ predictor_and_pdf_convert_setup/main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# setup docker first
+./setup_docker_ec2_cpu.sh
+
+# setup cortex
+
+
+# pull individual 3 repos
+
+
+# seed the environment variables in bashrc
+
+
+# activate the api activation protocal
+
+
+# set up crontab env
+
+
+# activate crontab
+
---
+++ setup_docker_ec2_cpu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' 
+
+sudo apt-get update
+printf "${GREEN}update libraries required for docker${NC}"
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+printf "${GREEN}download docker and add it's keys${NC}"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+printf "${GREEN}add repo for docke${NC}"
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+printf "${GREEN}install docke${NC}"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+
+sudo groupadd docker; sudo gpasswd -a $USER docker
+printf "${GREEN}\n\ninstalling cortex${NC}"
+$ bash -c "$(curl -sS https://raw.githubusercontent.com/cortexlabs/cortex/0.21/get-cli.sh)"
