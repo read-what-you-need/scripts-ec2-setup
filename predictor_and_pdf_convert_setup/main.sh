5a6df646883b5daf63db90ef86ff75ff693f572b
soulbliss <deepak.garasangi@gmail.com>
2021-04-10 16:50:23 +0530

    add gitignore

--- predictor_and_pdf_convert_setup/main.sh
+++ predictor_and_pdf_convert_setup/main.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 
-# setup docker first
-sudo bash ./setup_docker_ec2_cpu.sh
+# # setup docker first
+# sudo bash ./setup_docker_ec2_cpu.sh
 
-# setup cortex
+# # setup cortex
+# bash -c "$(curl -sS https://raw.githubusercontent.com/cortexlabs/cortex/0.21/get-cli.sh)"
 
+# # pull individual 3 repos
+# mkdir cortex-api
+# cd cortex-api
+# git clone https://github.com/read-what-you-need/pdf-to-text.git
+# git clone https://github.com/read-what-you-need/semantic-search-handler.git
+# git clone -r https://github.com/read-what-you-need/e2e-qg.git
 
-# pull individual 3 repos
 
 
 # seed the environment variables in bashrc
