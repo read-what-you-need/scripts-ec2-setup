6f7be01c9a8bfa85e6cf91fe344ee49c0880aadd
soulbliss <deepak.garasangi@gmail.com>
2021-04-14 16:23:30 +0530

    nginx certs

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,9 +1,11 @@
 sudo apt install -y nginx
-nginx -s
+
 sudo cp ./predictor_and_pdf_convert_setup/nginx_predictor_api.conf  /etc/nginx/conf.d/predictorapi.deeps.site.conf
 
-#sudo apt-get install python-certbot-nginx
+sudo apt-get install -y python-certbot-nginx
 
+# make sure the predictorapi.deeps.site has correct A records pointing to the ec2 instance
 #sudo nginx -t && nginx -s reload
-#sudo certbot --nginx -d predictorapi.deeps.site
+sudo certbot --nginx -d predictorapi.deeps.site
+
 #sudo service nginx restart
